76368c976f5aa14f75ec1a31f39612e0ff5ebd5c
ASUS <delenie1.3@gmail.com>
2021-10-10 11:15:15 +0500

    add func enter data

--- chat_bot.py
+++ chat_bot.py
@@ -12,3 +12,38 @@ def get_text_messages(message):#слушатель
         bot.send_message(message.from_user.id, 'Я тебя не понимаю. напиши /help.')
 
 bot.polling(non_stop=True, interval=0)#Запрос к телеграмму о сообщениях
+
+name = ''
+surname = ''
+age = 0
+
+@bot.message_handler(content_types=['text'])
+def start(message):#начало регистрации, имя
+    if message.text == '/reg':
+        bot.send_message(message.from_user.id, 'Как тебя зовут?')
+        bot.register_next_step_handler(message, get_name)
+    else:
+        bot.send_message(message.from_user.id, 'Напиши /reg')
+
+def get_name(message):#фамилия
+    global name
+    name = message.text
+    bot.send_message(message.from_user.id, 'Какая у тебя фамилия?')
+    bot.register_next_step_handler(message, get_surname)
+
+def get_surname(message):#возраст
+    global surname
+    surname = message.text
+    bot.send_message('Сколько тебе лет?')
+    bot.register_next_step_handler(message, get_age)
+
+def get_age(message):#проверка возраста
+    global age
+    while age == 0:
+        try:
+            age = int(message.text)
+        except Exception:
+            bot.send_message(message.from_user.id, 'Укажи возраст цифрами')
+    keyboard = types.InlineKeyboardMarkup()
+    key
+    bot.send_message(message.from_user.id, 'Тебе' + str(age) + ' лет, тебя зовут ' + name + ' ' + surname + '?')
