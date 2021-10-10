fb5ab2388eefe8acf1b6a8d2d6593917900661ad
ASUS <delenie1.3@gmail.com>
2021-10-10 11:48:31 +0500

    add call keyboard

--- chat_bot.py
+++ chat_bot.py
@@ -1,7 +1,8 @@
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot('2075149976:AAFSxgVfUtAgZ5ouuAiawN0V8X2QvPrBKDU')
-
+'''
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):#слушатель
     if message.text == 'привет':
@@ -9,7 +10,7 @@ def get_text_messages(message):#слушатель
     elif message.text == '/help':
         bot.send_message(message.from_user.id, 'Напиши привет')
     else:
-        bot.send_message(message.from_user.id, 'Я тебя не понимаю. напиши /help.')
+        bot.send_message(message.from_user.id, 'Я тебя не понимаю. напиши /help.')'''
 
 bot.polling(non_stop=True, interval=0)#Запрос к телеграмму о сообщениях
 
@@ -51,3 +52,15 @@ def get_age(message):#проверка возраста
     keyboard.add(key_no)
     question = 'Тебе' + str(age) + ' лет, тебя зовут ' + name + ' ' + surname + '?'
     bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):#оброботка ввода с клавиатуры
+    if call.data == 'yes':
+        bot.send_message(call.message.chat.id, 'Запомнил')
+    elif call.data == 'no':
+        bot.send_message(message.from_user.id, 'Как тебя зовут?')
+        bot.register_next_step_handler(message, get_name)
+        bot.send_message('Сколько тебе лет?')
+        bot.register_next_step_handler(message, get_age)
+        bot.send_message('Сколько тебе лет?')
+        bot.register_next_step_handler(message, get_age)
